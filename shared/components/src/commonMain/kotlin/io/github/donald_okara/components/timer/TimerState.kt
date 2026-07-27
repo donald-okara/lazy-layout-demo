82d3447cbee02c4ccd6a5f7bcd5e155f1c0a1854
donald-okara <donaldokara123@gmail.com>
2026-07-27 12:49:16 +0300

    update code snippets to support dark theme

--- shared/components/src/commonMain/kotlin/io/github/donald_okara/components/timer/TimerState.kt
+++ shared/components/src/commonMain/kotlin/io/github/donald_okara/components/timer/TimerState.kt
@@ -1,10 +1,11 @@
 package io.github.donald_okara.components.timer
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 data class TimerState(
     val status: TimerStatus = TimerStatus.Idle,
-    val totalTime: Duration = 10.seconds,
+    val totalTime: Duration = 45.minutes,
     val timeLeft: Duration = totalTime
 )
